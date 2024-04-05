35116c607742490a78b6189d0bfd42f84906513a
Văn Hiếu <vanhieu99it@gmail.com>
2024-04-05 15:15:55 +0700

    up spring-boot-hello-world

---
+++ spring-boot-hello-world/src/main/java/org/example/HelloController.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Controller
+// @ResponseBody
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World, Spring Boot!";
+    }
+
+}
