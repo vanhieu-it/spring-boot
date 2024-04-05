3688fd8feb21aa927d4351b8e814e7034837e74a
Văn Hiếu <vanhieu99it@gmail.com>
2024-04-05 15:33:03 +0700

    up spring-data-jpa

---
+++ spring-data-jpa/src/main/java/org/example/Book.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private LocalDate publishDate;
+
+    // for JPA only, no use
+    public Book() {
+    }
+
+    public Book(String title, BigDecimal price, LocalDate publishDate) {
+        this.title = title;
+        this.price = price;
+        this.publishDate = publishDate;
+    }
+
+}
---
+++ spring-data-jpa/src/main/java/org/example/Main.java
@@ -0,0 +1,87 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@SpringBootApplication
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    // Spring runs CommandLineRunner bean when Spring Boot App starts
+    @Bean
+    public CommandLineRunner demo(BookRepository bookRepository) {
+        return (args) -> {
+
+            Book b1 = new Book("Book A", BigDecimal.valueOf(9.99), LocalDate.of(2023, 8, 31));
+            Book b2 = new Book("Book B", BigDecimal.valueOf(19.99), LocalDate.of(2023, 7, 31));
+            Book b3 = new Book("Book C", BigDecimal.valueOf(29.99), LocalDate.of(2023, 6, 10));
+            Book b4 = new Book("Book D", BigDecimal.valueOf(39.99), LocalDate.of(2023, 5, 5));
+
+            // save a few books, ID auto increase, expect 1, 2, 3, 4
+            bookRepository.save(b1);
+            bookRepository.save(b2);
+            bookRepository.save(b3);
+            bookRepository.save(b4);
+
+            // find all books
+            log.info("findAll(), expect 4 books");
+            log.info("-------------------------------");
+            for (Book book : bookRepository.findAll()) {
+                log.info(book.toString());
+            }
+            log.info("\n");
+
+            // find book by ID
+            Optional<Book> optionalBook = bookRepository.findById(1L);
+            optionalBook.ifPresent(obj -> {
+                log.info("Book found with findById(1L):");
+                log.info("--------------------------------");
+                log.info(obj.toString());
+                log.info("\n");
+            });
+
+            // find book by title
+            log.info("Book found with findByTitle('Book B')");
+            log.info("--------------------------------------------");
+            bookRepository.findByTitle("Book C").forEach(b -> {
+                log.info(b.toString());
+                log.info("\n");
+            });
+
+            // find book by published date after
+            log.info("Book found with findByPublishedDateAfter(), after 2023/7/1");
+            log.info("--------------------------------------------");
+            bookRepository.findByPublishedDateAfter(LocalDate.of(2023, 7, 1)).forEach(b -> {
+                log.info(b.toString());
+                log.info("\n");
+            });
+
+            // delete a book
+            bookRepository.deleteById(2L);
+            log.info("Book delete where ID = 2L");
+            log.info("--------------------------------------------");
+            // find all books
+            log.info("findAll() again, expect 3 books");
+            log.info("-------------------------------");
+            for (Book book : bookRepository.findAll()) {
+                log.info(book.toString());
+            }
+            log.info("\n");
+
+        };
+    }
+
+}
