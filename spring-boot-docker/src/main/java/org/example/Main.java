a88f1e9ba1036b4c1f856ed388756645b13ebad4
Văn Hiếu <vanhieu99it@gmail.com>
2024-04-09 09:18:59 +0700

    docker

---
+++ spring-boot-docker/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class Main {
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Docker + Spring Boot");
+        model.addAttribute("msg", "Welcome to the docker container!");
+        return "index";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
